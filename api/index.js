9bd5e10f98dd5cc5cb22e579efcbac0cb260017c
MangeshChaudhari2000 <157056611+MangeshChaudhari2000@users.noreply.github.com>
2024-11-07 23:36:16 +0530

    fixed path issue

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.MONGO);
 
 import express from "express";
 import mongoose from "mongoose";
@@ -8,13 +7,14 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected to DB");
 }).catch((err) => {
     console.log("error connecting to mongoDB: ", err.message);
 })
 
-const _direname = path.resolve();
+const __direname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -31,9 +31,9 @@ app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
 
-app.use(express.static(pah.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__direname, '/client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.join(__direname, 'client', 'dist', 'index.html'))
 })
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
